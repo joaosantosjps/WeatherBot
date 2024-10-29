60d22d7f2c8baabaccf92a37d1c696c9b92565be
joaosantosjps <joaosantos270899@outlook.com>
2024-10-29 22:03:28 +0000

    Spider e Items finalizados

--- weatherbot_data/spiders/Meteored.py
+++ weatherbot_data/spiders/Meteored.py
@@ -1,10 +1,11 @@
 import scrapy
-
+from weatherbot_data.items import WeatherbotDataItem
 
 class MeteoredSpider(scrapy.Spider):
     name = "Meteored"
     domain = "https://www.tempo.pt"
     city = "moimenta-da-beira"
+    
 
     def start_requests(self):
         yield scrapy.Request(
@@ -15,40 +16,33 @@ class MeteoredSpider(scrapy.Spider):
     def get_forecast_by_week(self, response):
         block = response.xpath('//ul[@class="grid-container-7 dias_w"]')
         
-        day = block.xpath('//span[@class="subtitle-m"]/text()').extract()
-        max_temperature = block.xpath('//span[@class="max changeUnitT"]/text()').extract()
-        min_temperature = block.xpath('//span[@class="min changeUnitT"]/text()').extract()
-        wind_speed = block.xpath('//span[@class="changeUnitW"]/text()').extract()
+        days = block.xpath('//span[@class="subtitle-m"]/text()').extract()
+        max_temps = block.xpath('//span[@class="max changeUnitT"]/text()').extract()
+        min_temps = block.xpath('//span[@class="min changeUnitT"]/text()').extract()
+        wind_speeds = block.xpath('//span[@class="changeUnitW"]/text()').extract()
 
-        path = block.xpath('//span[@class="prediccion col"]/span[@class="precip col"]')
-        porcent_rain = path.xpath('span[@class="txt-strng probabilidad center"]/text()').extract()
-        aumat_rain = path.xpath('span[@class="changeUnitR"]/text()').extract_first()
+        precipitation_path = block.xpath('//span[@class="prediccion col"]/span[@class="precip col"]')
+        rain_probabilities = precipitation_path.xpath('span[@class="txt-strng probabilidad center"]/text()').extract()
+        rain_amounts = precipitation_path.xpath('span[@class="changeUnitR"]/text()').extract()
+
+        processed_data = self.processing(days, max_temps, min_temps, wind_speeds, rain_probabilities, rain_amounts)
         
-        collected = {
-            "day": day,
-            "max_temperature": self.processing_temp(max_temperature),
-            "min_temperature": self.processing_temp(min_temperature),
-            "wind_speed": self.processing_wind(wind_speed),
-            "porcent_rain": porcent_rain,
-            "aumat_rain": aumat_rain
-        }
+        yield WeatherbotDataItem(processed_data)
 
-        yield collected
+    def processing(self, days, max_temps, min_temps, wind_speeds, rain_probabilities, rain_amounts):
+        processed = {}
 
-    def processing_temp(self, info, tag):
-        for data in info:
-            print(data.replace("°", ""))
+        processed["days"] = str([string.replace(".", "") for string in days ])
+
+        processed["max_temps"] = [int(num.replace("°", "")) for num in max_temps]
         
-    def processing_wind(self, info):
-        for data in info:
-            print(data.replace("km/h", ""))
-        
-        """for data in collected["wind_speed"]:
-            print(data)
+        processed["min_temps"] = ([int(num.replace("°", "")) for num in min_temps])
+    
+        cleaned_speeds = [int(speed.strip()) for speed in wind_speeds if speed.strip().isdigit()]
+        processed["wind_speeds"] = [[cleaned_speeds[i], cleaned_speeds[i+1]] for i in range(0, len(cleaned_speeds) - 1, 2)][:7]
+    
+        processed["rain_probabilities"] = [int(num.replace("%", "")) for num in rain_probabilities]
+    
+        processed["rain_amounts"] = [float(num.replace("mm", "")) for num in rain_amounts]     
 
-        for data in collected["porcent_rain"]:
-            print(data)
-
-        for data in collected["porcent_rain"]:
-            print(data)"""
-
+        return processed
